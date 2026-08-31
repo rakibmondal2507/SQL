9b626676ce0daa1037bd82069eabd84cc42eb38e
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2026-08-31 10:39:21 +0530

    Add SQL query to find average score by country

---
+++ dataset/aggregate/HAVING/one.sql
@@ -0,0 +1,9 @@
+-- Find the average score for each country considering only cutomers with a score not equal to 0 and return only those countries with an average score greater than 430
+
+SELECT 
+country,
+AVG (score) AS avg_score
+FROM customers
+WHERE score !=0
+GROUP BY country
+HAVING AVG (score) > 430

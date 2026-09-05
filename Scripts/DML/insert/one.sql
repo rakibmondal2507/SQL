7c9a14ceafd1e00c522f120ff1b28a71278eb69e
Rakib Mondal <134124152+rakibmondal2507@users.noreply.github.com>
2026-09-05 06:46:23 +0530

    Add insert statements for customers in one.sql
    Added SQL insert statements for new customers with cautionary notes.

---
+++ Scripts/DML/insert/one.sql
@@ -0,0 +1,9 @@
+INSERT INTO customers(id, first_name, country, score)
+VALUES  (6, 'Rakib', 'India', Null),
+		(7, 'Payel', Null, 432)
+
+-- Caution : Columns and value must be same order.
+-- Rules : Matching Data types , column Count & Constraints
+-- Note : You can skip the column if you insert value for every column
+-- Tip : Always list columns explictily for clarity and maintainablility
+-- Note : Column not included in INSERT become NULL (useless a default constraint exist)
